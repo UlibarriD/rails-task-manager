6507999074603fcf7f1a01c3601ae9a00c8045c5
Diego Ulibarri <diegoulhe@outlook.com>
2019-05-14 15:37:27 -0400

    Modified the routes so we can create, read and update the tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,35 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    new_task = Task.new(allowed_params)
+    new_task.save
+    redirect_to '/tasks'
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(allowed_params)
+    redirect_to "/tasks/#{params[:id]}"
+  end
+
+  private
+
+  def allowed_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
